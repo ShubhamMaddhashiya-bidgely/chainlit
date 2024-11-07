8b3c0f0046d58ddd16ed43a1c34dbbd8e07cdcfa
Shubham Maddhashiya <shubhamsipah00@bidgely.com>
2024-11-07 20:08:00 +0530

    convert it to standalone pkg

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['npx prettier --write', 'npx eslint --fix'],
-  '**/*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit'],
-  '**/*.py': ['ruff check --fix', 'ruff format', () => 'pnpm run lintPython'],
-  '.github/{workflows,actions}/**': ['actionlint']
-};
